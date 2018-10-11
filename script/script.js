58d8ef25a9991e8bebb5f99e1176b1b474bbaf14
mathiasLoiret <mathias44.loiret@hotmail.fr>
2018-10-11 14:14:00 +0200

    changing to iFrame

---
+++ script/script.js
@@ -0,0 +1,8 @@
+
+function loadMap(){
+    document.getElementById("contenu").innerHTML = " <iframe src=\"./maps.html\" class=\"iframeCustom\"></iframe> ";
+}
+
+function loadListe(){
+    document.getElementById("contenu").innerHTML = " <iframe src=\"./Liste.html\" class=\"iframeCustom\"></iframe> ";
+}
